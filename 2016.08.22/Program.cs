71d0a780c31bbc3e5c50b5ab81706bd490030549
Andrey Soltan <stigmataandrey@gmail.com>
2016-08-22 11:36:27 +0300

    Delegates and user subscription

---
+++ 2016.08.22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016._08._22
+{
+    public delegate void myDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Event ev = new Event();
+            User u = new User("Test user", 20);
+            //sub user to events
+            ev.myEvent+=u.Handler;
+            ev.onEventOccurred();
+        }
+    }
+}
---
+++ 2016.08.22/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2016._08._22
+{
+    public class User
+    
+    {
+        string name;
+        int age;
+
+        public User(string name, int age)
+        {
+            this.age = age;
+            this.name = name;
+        }
+        public string Name
+        {
+            get { return name; }
+            set{ name=value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age=value;}
+        }
+
+        public void Handler()
+        {
+            Console.WriteLine("User:{0}, age:{1} logged in", name, age);
+        }
+    }
+}
