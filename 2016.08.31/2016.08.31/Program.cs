3c5434d24a6225fecf4df354dfc16dabf0cb6592
Andrey Soltan <stigmataandrey@gmail.com>
2016-08-31 10:18:52 +0300

    reading XML data

--- 2016.08.31/2016.08.31/Program.cs
+++ 2016.08.31/2016.08.31/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace _2016._08._31
 {
@@ -10,6 +11,24 @@ namespace _2016._08._31
     {
         static void Main(string[] args)
         {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"../../Data/data1.xml");
+
+            XmlNode root = doc.DocumentElement;
+            //show data
+            XmlNodeList nodes = doc.GetElementsByTagName("student");
+            
+            foreach(XmlNode n in nodes)
+            {
+                // look through names
+                //Console.WriteLine("{0}", n.Name);
+                XmlAttributeCollection attribute = n.Attributes;
+                foreach(XmlNode a in attribute)
+                {
+                    Console.Write("{0} ", a.Value);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
