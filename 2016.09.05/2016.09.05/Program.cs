87b283be063725a631824125ae2db89805f29601
Andrey Soltan <stigmataandrey@gmail.com>
2016-09-05 10:06:43 +0300

    read from XML by Attribute

--- 2016.09.05/2016.09.05/Program.cs
+++ 2016.09.05/2016.09.05/Program.cs
@@ -14,19 +14,27 @@ namespace _2016._09._05
             XmlTextReader reader = new XmlTextReader(@"../../Data/data1.xml");
             reader.WhitespaceHandling = WhitespaceHandling.None;
 
-            while(reader.Read())
+            while (reader.Read())
             {
-               XmlNodeType type = reader.NodeType;
-               string name = reader.Name;
-               string value = reader.Value;
-               int q = reader.AttributeCount;
-                while (reader.MoveToNextAttribute())
+                XmlNodeType type = reader.NodeType;
+                string name = reader.Name;
+                string value = reader.Value;
+                int q = reader.AttributeCount;
+
+                //Console.WriteLine("id:{0}\t\ttitle={1}\t\t",reader.Name, reader.Value);
+
+                if (reader.NodeType==XmlNodeType.Element && reader.AttributeCount > 0)
                 {
-
+                    Console.WriteLine(reader.GetAttribute("id"));
+                    Console.WriteLine(reader.GetAttribute("title"));
+                    Console.WriteLine(reader.GetAttribute(" "));
                 }
-
-                reader.Close();
+            
             }
+
+            reader.Close();
+
+
         }
     }
 }
