80aa85bca4c77c2e3b483295de3047acb46107c5
Andrey Soltan <stigmataandrey@gmail.com>
2016-09-05 09:39:53 +0300

    added filled XML file and determine it's methods

--- 2016.09.05/2016.09.05/Program.cs
+++ 2016.09.05/2016.09.05/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace _2016._09._05
 {
@@ -10,6 +11,22 @@ namespace _2016._09._05
     {
         static void Main(string[] args)
         {
+            XmlTextReader reader = new XmlTextReader(@"../../Data/data1.xml");
+            reader.WhitespaceHandling = WhitespaceHandling.None;
+
+            while(reader.Read())
+            {
+               XmlNodeType type = reader.NodeType;
+               string name = reader.Name;
+               string value = reader.Value;
+               int q = reader.AttributeCount;
+                while (reader.MoveToNextAttribute())
+                {
+
+                }
+
+                reader.Close();
+            }
         }
     }
 }
