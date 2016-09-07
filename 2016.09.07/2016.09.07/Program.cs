5b24cd84675817c2ddc4032234285d5014877eaa
Andrey Soltan <stigmataandrey@gmail.com>
2016-09-07 10:47:35 +0300

    FileStream connection

--- 2016.09.07/2016.09.07/Program.cs
+++ 2016.09.07/2016.09.07/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
 namespace _2016._09._07
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Student s = new Student("Student Names", 21, 7.5);
+            FileStream fs = new FileStream(@"..\..\student.dat",FileMode.Create,FileAccess.Write,FileShare.None);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs,s);
+            fs.Close();
+            Console.WriteLine("Worked");
         }
     }
 }
---
+++ 2016.09.07/2016.09.07/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace _2016._09._07
+{
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Rate { get; set; }
+        public Student(string name, int age, double rate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Rate = rate;
+        }
+        public override string ToString()
+        {
+            return String.Format(
+                "name:{0}" +
+                "age:{1}" +
+                "rate:{2}" +
+                Name, Age, Rate);
+        }
+    }
+}
